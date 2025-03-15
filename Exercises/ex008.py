91159c2d78ffcb88b098aef8edff4d3d886c7444
MuhammedElubeyid <eprrmkz12345@gmail.com>
2025-03-15 03:52:10 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,10 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 220543603
+# Your Name and Surname: Muhammed Elubeyid
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n + 1):
+    
+    print(" " * (n - i), end="")
+    
+    print("*" * (2 * i - 1))
