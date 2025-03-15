d017db6dcb579860d40dc1240ba6dff81bf3f0d5
MuhammedElubeyid <eprrmkz12345@gmail.com>
2025-03-15 21:06:18 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -8,3 +8,12 @@ for i in range(1, n + 1):
     print(" " * (n - i), end="")
     
     print("*" * (2 * i - 1))
+## Second way 
+for i in range(1,6):
+    for j in range(i,6):
+        print(end=' ')
+    for j in range(1,i+ 1):
+        print('*', end='')
+    for j in range(1,i):
+     print('*', end='')
+    print()
