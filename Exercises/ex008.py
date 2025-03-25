d57b8838214915b355dd93eb8efcd0f324a80e58
MuhammedElubeyid <eprrmkz12345@gmail.com>
2025-03-25 17:19:32 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -11,7 +11,7 @@ for i in range(1, n + 1):
 ## Second way 
 n = int(input("Enter the number of rows: "))
 for i in range(1,n+ 1):
-    for j in range(i,6):
+    for j in range(i,n):
         print(end=' ')
     for j in range(1,i+ 1):
         print('*', end='')
