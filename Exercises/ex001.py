9fc59dfcacc304d84fd5a97ab40d2f0fc09b252b
MuhammedElubeyid <eprrmkz12345@gmail.com>
2025-03-25 14:51:15 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,4 +1,4 @@
 # Your Student ID: 220543603
 # Your Name and Surname: Muhammed Elubeyid
 
-print("Twinkle, twinkle, little star, \n\t\tHow I wonder what you are! \n\t\t\tUp above the world so high, \n\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star\n\t How I wonder what you are")
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star\n\t How I wonder what you are")
